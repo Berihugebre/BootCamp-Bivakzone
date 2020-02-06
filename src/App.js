3d1df07f8d8ed3964e9eba7c90e87b4407139a52
Anthony Meirlaen <anthony.meirlaen@gmail.com>
2020-02-06 10:50:53 +0100

    Load file with case sensitive path
    Mac filesystem is case sensitive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import Layout from './components/Layout';
+import Layout from './Components/Layout';
 import {LocalizeProvider} from 'react-localize-redux'
 
 function App() {
